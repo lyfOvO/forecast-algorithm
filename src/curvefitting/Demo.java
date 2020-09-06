6103be93560ae615228111eeff87c1ff63395b27
lyfOvO <1003555715@qq.com>
2020-09-06 15:19:32 +0800

    修正了日期间隔的计算，并且完成了预测模型的框架

--- src/curvefitting/Demo.java
+++ src/curvefitting/Demo.java
@@ -1,7 +1,10 @@
 package curvefitting;
 
 public class Demo {
-    Fitting test=new Fitting();
+    public static void main(String[] args){
+        Fitting test=new Fitting();
+        int x=test.getStartX("2020-7-10",10);
+    }
 
 
 }
--- src/curvefitting/Fitting.java
+++ src/curvefitting/Fitting.java
@@ -2,6 +2,7 @@ package curvefitting;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +15,49 @@ public class Fitting {
     List<Integer> y; //用于预测的疫情数据
 
     Fitting(){
+        List<Integer> prediction=new ArrayList<>();
+        if(!hasControl){
+            //不进行控制的自然增长模型
+        }
+        else{
+            int today=y.size();
+            System.out.println("today:"+today);
+            int start=this.getStartX(startControlDate,today);
+            if(today<start){
+                //today处于①
+                if(start-today>=num){
+                    //整个预测时期(start~start+num)都处于①
+                }
+                else{
+                    //预测时期超过①
+                    if(num-(start-today)<=last){
+                        //预测时期仅停留在①②
+                        //①today~start
+                        //②start~num-(start-today)
+                    }
+                    else{
+                        //预测时期处于①②③
+                        //①today~start
+                        //②start~start+last
+                        //③start+last~num+today
+                    }
+                }
+            }
+            else if(today<start+last){
+                //今天处于②
+                if(today+num>start+last){
+                    //预测时期处于②③阶段
+                    //②today~start+last
+                    //③start+last~today+num
+                }
+                else{
+                    //预测时期仅处于②today~today+num
+                }
+            }
+            else{
+                //今天处于③，预测时期也只会处于③today~today+num
+            }
+        }
 
     }
 
@@ -38,21 +82,17 @@ public class Fitting {
     }
 
     public int getStartX(String startControlDate,int today){
-        int distance=0;
+        long distance=0;
         int start=0;
         //获取开始进行政策控制的日期与今天日期的天数差
         try {
             Date date=new SimpleDateFormat("yyyy-MM-dd").parse(startControlDate);
             Date todayDate=new Date();
-            System.out.println("today:"+todayDate);
-            System.out.println("start:"+date);
-            distance= (int) (date.getTime()-todayDate.getTime())/(24*60*60*1000);
-            System.out.println("controlStartDate - today="+distance);
-
+            distance= (long) (date.getTime()-todayDate.getTime())/(24*60*60*1000);
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
+        System.out.println("control start date:"+(today+distance));
         return start;
     }
 
